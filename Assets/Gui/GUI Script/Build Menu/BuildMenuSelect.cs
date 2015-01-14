00f007924be77608a6d7e5ce2c105b63e214d891
Phillip Robertson <offbeatspartan@gmail.com>
2015-01-13 17:03:17 -0700

    [0.6.1] ReAdded gui and added proper animations for Menu slide.

---
+++ Assets/Gui/GUI Script/Build Menu/BuildMenuSelect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildMenuSelect : MonoBehaviour {
+
+	public Animator BuildMenu;
+
+	// Update is called once per frame
+	public void Anim () {
+		if (BuildMenu.GetBool ("isHidden") == false) {
+				BuildMenu.SetBool ("isHidden", true);
+
+				}
+		else  {
+			BuildMenu.SetBool ("isHidden", false);	
+		
+		}
+
+		
+		
+		
+
+	}
+}
