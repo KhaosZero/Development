2a697cf6eb019a51bed9607e915e0a674d6c50de
Phillip Robertson <offbeatspartan@gmail.com>
2015-02-12 22:47:19 -0700

    [0.6.2] Added anims and continued work on pathfinding

---
+++ Assets/Characters/Scripts/Tran.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tran : MonoBehaviour {
+
+
+	float Speed;
+
+	NavMeshAgent agent;
+	public Animator Transition;
+	Rigidbody Char;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		Char = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+
+
+	void Update () {
+		Speed = (Char.velocity.x);
+		print (Speed);
+
+
+		if (Speed > 1 ) {
+			Transition.SetBool("Walking", true);
+			
+		}
+		else  {
+			Transition.SetBool("Walking", false);
+			
+		}
+	}
+}
